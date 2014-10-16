20c4c2fec39685e0c51466bdfee3b9449995186f
balasan <byslava@gmail.com>
2014-10-16 19:45:28 -0400

    initial button

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,12 +1,24 @@
 'use strict';
 
 angular.module('contraceptionApp')
-  .controller('MainCtrl', function ($scope, $http, Auth,$location) {
+  .controller('MainCtrl', function ($scope, $http, Auth,$location,$state) {
 
     $scope.startServey = function(){
 
-      if(Auth.isLoggedIn())
-        $location.url('/questions/intro/1')
+      if(Auth.isLoggedIn()){
+
+        var user = Auth.getCurrentUser().$promise.then(function(user){
+
+          var section = user.currentSection
+          if(section == undefined)
+            $location.url('/questions/intro/1')
+          else{
+            $state.go('sections.questions', {type:'question',id:section})
+          }
+
+
+        })
+      }
       else
         $location.url('/signup')
 
